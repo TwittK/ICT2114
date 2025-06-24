7739afbc8f84f7d7e0ae31f8bc01bec13a1b31bf
kellyjw030 <kellyjw2002@gmail.com>
2025-06-24 23:42:28 +0800

    Update detection_pose.py

--- detection/detection_pose.py
+++ detection/detection_pose.py
@@ -78,11 +78,11 @@ def extract_face_from_nose(pose_points, frame):
 
     # add one unit (of nose_to_eye_height) above eye center, one below nose
     y1 = max(int(eye_center_y - nose_to_eye_height), 0)
-    y2 = min(int(nose[1] + nose_to_eye_height), h)
+    y2 = min(int(nose[1] + nose_to_eye_height) + 20, h)
 
     # horizontal distance of bbox based on ears
-    x1 = max(int(min(l_ear[0], r_ear[0])), r_eye[0] - 10)
-    x2 = min(int(max(l_ear[0], r_ear[0])), l_eye[0] + 10)
+    x1 = max(int(min(l_ear[0], r_ear[0])), r_eye[0] - 40)
+    x2 = min(int(max(l_ear[0], r_ear[0])), l_eye[0] + 40)
 
     # check that bbox is valid
     if x2 <= x1 or y2 <= y1:
