a97971d3d376364f481e6acd0ab947eb823436a9
DericM-hub <132824207+DericM-hub@users.noreply.github.com>
2025-07-14 14:35:51 +0800

    Test food model on different thread

--- modularized/app.py
+++ modularized/app.py
@@ -16,22 +16,28 @@ os.makedirs(os.path.join("web", "static", "incompliances"), exist_ok=True)
 os.makedirs("yolo_models", exist_ok=True)
 
 drink_model = YOLO(os.path.join("yolo_models", "yolo11n.pt"))
+food_model = YOLO(os.path.join("yolo_models", "food.pt"))
 pose_model = YOLO(os.path.join("yolo_models", "yolov8n-pose.pt"))
-classif_model = YOLO(os.path.join("yolo_models", "yolov8n-cls.pt"))
+classif_model = YOLO(os.path.join("yolo_models", "yolo11n-cls.pt"))
+# classif_model = YOLO(os.path.join("yolo_models", "yolov8n-cls.pt"))
 target_class_list = [39, 40, 41, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55]
 
-# [39, 40, 41]
-# just the drinks from coco dataset
 # bread: 31, chips: 55, chocolate: 56, cookies: 67, desserts: 77, french fries: 86, hamburger: 100, ice-cream: 108, pastry: 156, waffles: 231
-# food_class_list = [31, 55, 56, 67, 77, 86, 100, 108, 156, 231]
+food_class_list = [31, 55, 56, 67, 77, 86, 100, 108, 156, 231]
 
 if __name__ == "__main__":
 
     try:
-        read_thread = threading.Thread(target=read_frames, args=(True, "192.168.10.64", "101"))
+        read_thread = threading.Thread(target=read_frames, args=(False, "192.168.10.64", "101"))
         inference_thread = threading.Thread(
             target=preprocess,
             args=(drink_model, pose_model, target_class_list, 0.3, classif_model),
+            daemon=True,
+        )
+        # food model preprocess
+        inference_food_thread = threading.Thread(
+            target=preprocess,
+            args=(food_model, pose_model, food_class_list, 0.3, classif_model),
             daemon=True,
         )
         detection_thread = threading.Thread(target=detection)
@@ -40,6 +46,7 @@ if __name__ == "__main__":
 
         read_thread.start()
         inference_thread.start()
+        inference_food_thread.start()
         detection_thread.start()
         save_thread.start()
         flask_thread.start()
